6c4791c0194f8b22a1c035e86f8c76466b15aa19
Grace_T <yun65227@163.com>
2018-04-12 23:29:47 +0800

    Update ready.py

--- ready.py
+++ ready.py
@@ -2,7 +2,7 @@ import shelve
 import Myclass as mc
 import MySetting as ms
 
-gigaVocab = mc.GigaLoader(0, 100000)
+gigaVocab = mc.GigaLoader(0, 5089620)
 
 lang = mc.Lang('lang')
 lang.index_giga(gigaVocab)
@@ -12,7 +12,7 @@ obj_file.close()
 
 vocab_size = lang.n_words
 
-wv = mc.wordVector(ms.wv_Path, lang.word2index, size=100000)
+wv = mc.wordVector(ms.wv_Path, lang.word2index, size=5089620)
 enco = mc.Encoder(vocab_size, ms.h_size, wv.veclist, n_layers=1)
 deco = mc.AttnDecoder(hidden_size=ms.h_size, output_size=vocab_size, n_layers=1)
 enco.save_para(ms.net_Path, "mini_Enco.pkl")
